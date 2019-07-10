ba0556b85925a54ffbdb9593f830fea869fa672c
Guilherme Uberti <guilherme.uberti@hotmail.com>
2019-07-10 15:06:57 -0300

    interceptando http request de angular-in-memory-web-api

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRountingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { HttpClientInMemoryWebApiModule } from "angular-in-memory-web-api";
+import { InMemoryDatabase } from "./in-memory-database";
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRountingModule
+    AppRountingModule,
+    HttpClientInMemoryWebApiModule.forRoot(InMemoryDatabase)
   ],
   providers: [],
   bootstrap: [AppComponent]
